d83ab58f627a97820b2315f42af0d1e12b3916da
praveshpansari <praveshpansari@gmail.com>
2023-07-08 20:14:43 +0530

    feat: cd command

---
+++ src/components/terminal/commands/cd.tsx
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+
+const cd = (args: string[], setPrompt: Function, setOutput: Function) => {
+  setPrompt("/praveshpansari.github.com/home:~$  ");
+};
+
+export default cd;
