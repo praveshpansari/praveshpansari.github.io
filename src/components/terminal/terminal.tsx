98030cf7657a72fb15fb6e64f8dd5ea254bfd9dd
praveshpansari <praveshpansari@gmail.com>
2023-07-12 20:34:27 +0530

    feat: initial commands auto

--- src/components/terminal/terminal.tsx
+++ src/components/terminal/terminal.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import styles from './terminal.module.css';
 import { IllegalArgumentError, commands } from './terminal.model';
 
@@ -7,53 +7,83 @@ const Terminal = () => {
   const [output, setOutput] = useState<ReactElement>(<></>);
   const [command, setCommand] = useState('');
   const [prompt, setPrompt] = useState('/praveshpansari.github.io:~$  ');
+  const [load, setLoad] = useState(false);
 
-  const executeCommand = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      setOutput((prev) => (
-        <>
-          {prev}
-          <div className={styles.location}>
-            {prompt}
-            <span className={styles.command}>{command}</span>
-          </div>
-        </>
-      ));
-      const commandArray = command.split(' ');
+  const inputEl = useRef<HTMLInputElement>(null);
 
-      try {
-        if (commandArray[0] === 'clear') {
-          if (commandArray.length > 1) throw new IllegalArgumentError();
-          setOutput(<></>);
-        } else if (commandArray[0] === 'cd') {
-          setPrompt('/praveshpansari.github.io/home:~$  ');
-        } else {
-          if (commands.has(commandArray[0]))
-            setOutput((prev) => (
-              <>
-                {prev}
-                {commands.get(commandArray[0])?.invoke?.(commandArray.splice(1))}
-              </>
-            ));
-          else throw new TypeError();
-        }
-      } catch (e) {
-        let error = '';
+  const updateOutput = (command: string) => {
+    setOutput((prev) => (
+      <>
+        {prev}
+        <div className={styles.location}>
+          {prompt}
+          <span className={styles.command}>{command}</span>
+        </div>
+      </>
+    ));
+  };
+
+  const executeCommand = (command: string) => {
+    updateOutput(command);
+    const commandArray = command.split(' ');
+
+    try {
+      if (commandArray[0] === 'clear') {
+        if (commandArray.length > 1) throw new IllegalArgumentError();
+        setOutput(<></>);
+      } else if (commandArray[0] === 'cd') {
+        setOutput((prev) => (
+          <>
+            {prev}
+            This command is currently locked.
+          </>
+        ));
+      } else {
+        if (commands.has(commandArray[0]))
+          setOutput((prev) => (
+            <>
+              {prev}
+              {commands.get(commandArray[0])?.invoke?.(commandArray.splice(1))}
+            </>
+          ));
+        else throw new TypeError();
+      }
+    } catch (e) {
+      let error = '';
+      if (command !== '') {
         if (e instanceof TypeError) {
           error = `"${command}" Invalid command. Please use "help" command for the available list of commands.`;
         } else if (e instanceof IllegalArgumentError) {
           error = `Usage: ${commands.get(commandArray[0])?.format}`;
         }
-        setOutput((prev) => (
-          <>
-            {prev}
-            {error}
-          </>
-        ));
       }
-      setCommand('');
+      setOutput((prev) => (
+        <>
+          {prev}
+          {error}
+        </>
+      ));
     }
+    setCommand('');
   };
+
+  useEffect(() => {
+    setCommand('cd');
+    updateOutput('cd');
+    setPrompt('/praveshpansari.github.io/home:~$  ');
+    setLoad(true);
+  }, []);
+
+  useEffect(() => {
+    if (load) {
+      setCommand('profile');
+      executeCommand('profile');
+    }
+  }, [load]);
+
+  useEffect(() => {
+    inputEl.current?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' });
+  });
 
   return (
     <div className={styles.main}>
@@ -62,11 +92,13 @@ const Terminal = () => {
         <div className={styles.location}>{prompt}</div>
         <input
           type="text"
+          ref={inputEl}
           className={styles.input}
           value={command}
           autoFocus
-          onKeyDown={executeCommand}
-          // onBlur={(e) => e.target.focus()}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') executeCommand(command);
+          }}
           onChange={(e) => setCommand(e.target.value)}
         ></input>
       </div>
