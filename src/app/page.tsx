0b72f5e40eabbffd1a312dd75c7b402d78a11e6c
Pravesh Pansari <praveshpansari@gmail.com>
2023-07-14 18:43:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,18 @@ import Terminal from '@/components/terminal/terminal';
 
 export default function Home() {
   return (
+    <>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0D9C3K7R6D"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-0D9C3K7R6D');
+    </script>
     <main className={styles.main}>
       <Terminal />
     </main>
+      </>
   );
 }
