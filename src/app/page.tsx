5b049d30857ba7c7a62e61b06497aa21911faa1d
praveshpansari <praveshpansari@gmail.com>
2023-07-14 18:47:17 +0530

    refactor: ganalytics bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
+import Script from 'next/script';
 import styles from './page.module.css';
 import Terminal from '@/components/terminal/terminal';
 
 export default function Home() {
   return (
     <>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0D9C3K7R6D"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-0D9C3K7R6D');
-    </script>
-    <main className={styles.main}>
-      <Terminal />
-    </main>
-      </>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-0D9C3K7R6D"></Script>
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-0D9C3K7R6D');
+        `}
+      </Script>
+      <main className={styles.main}>
+        <Terminal />
+      </main>
+    </>
   );
 }
